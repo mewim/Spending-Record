892f43d494c091c70121c513d44741f1977068dd
Liu, Chang <cliu81@illinois.edu>
2017-04-11 06:16:46 -0500

    List done.

---
+++ public/assets/js/list.js
@@ -0,0 +1,47 @@
+// Init global variables
+var records_table = null;
+/**
+ * Add record to the table
+ * @param {Object} record
+ */
+
+const add_record = function (record) {
+    var location = record.location ? $('<p>').text(record.location).html() : '(Not Available)';
+    records_table.row.add([
+        $('<p>').text(record.description).html(),
+        location,
+        record.amount,
+        $('<p>').text(record.category).html(),
+        moment(record.date).format('YYYY-MM-DD HH:mm'),
+    ]).draw();
+};
+
+// Construct jQuery datatable when document is ready
+$(document).ready(function () {
+    records_table = $('#records').DataTable({
+        "columnDefs": [
+            {"orderable": false, "targets": 4}
+        ]
+    });
+    $.ajax({
+        url: '/api/record/get_all',
+        type: 'POST',
+        data: JSON.stringify({
+            token: localStorage.getItem('token')
+        }),
+        contentType: "application/json; charset=utf-8",
+        success: function (records) {
+            for(var i=0; i<records.length; ++i){
+                add_record(records[i]);
+            }
+        },
+        error: function (xhr, status, error) {
+            if(xhr.status == 401){
+                window.location = '/signout'
+            }
+            else {
+                alert('There is a network error, please try again later.');
+            }
+        }
+    });
+});
