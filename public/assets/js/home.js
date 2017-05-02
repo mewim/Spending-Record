ab4eb1db009373615480ebe1320be074256a0f71
Liu, Chang <cliu81@illinois.edu>
2017-05-02 15:12:20 -0500

    Fix home page issue.

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -24,38 +24,36 @@ angular.module('Home', []).controller('HomeController', function ($scope, $http)
         function (res) {
             // success callback
             $scope.user = res.data;
-        },
-        function (res) {
-            // failure callback
-            if (res.status == 401) {
-                window.location = '/signout'
-            }
-            else {
-                alert("Sorry. Bad connection. Please try again.");
-            }
-        });
-
-    var date = new Date();
-
-    // Get spending information from server
-    $http({
-        method: 'POST',
-        url: '/api/record/get_range',
-        data: {
-            token: localStorage.getItem('token'),
-            start: date_to_string(new Date(date.getFullYear(), date.getMonth(), 1)),
-            end: date_to_string(new Date(date.getFullYear(), date.getMonth() + 1, 0))
-        }
-    }).then(
-        function (res) {
-            console.log(res);
-            // success callback
-            var money_spent = 0;
-            for(var i = 0; i < res.data.records.length; ++i){
-                money_spent += res.data.records[i].amount;
-            }
-            $scope.money_spent = money_spent.toFixed(2);
-            $scope.utilization_ratio = ((money_spent / $scope.user.budget) * 100).toFixed(2);
+            // Get spending information from server
+            var date = new Date();
+            $http({
+                method: 'POST',
+                url: '/api/record/get_range',
+                data: {
+                    token: localStorage.getItem('token'),
+                    start: date_to_string(new Date(date.getFullYear(), date.getMonth(), 1)),
+                    end: date_to_string(new Date(date.getFullYear(), date.getMonth() + 1, 0))
+                }
+            }).then(
+                function (res) {
+                    console.log(res);
+                    // success callback
+                    var money_spent = 0;
+                    for (var i = 0; i < res.data.records.length; ++i) {
+                        money_spent += res.data.records[i].amount;
+                    }
+                    $scope.money_spent = money_spent.toFixed(2);
+                    $scope.utilization_ratio = ((money_spent / $scope.user.budget) * 100).toFixed(2);
+                },
+                function (res) {
+                    // failure callback
+                    if (res.status == 401) {
+                        window.location = '/signout'
+                    }
+                    else {
+                        alert("Sorry. Bad connection. Please try again.");
+                    }
+                });
         },
         function (res) {
             // failure callback
